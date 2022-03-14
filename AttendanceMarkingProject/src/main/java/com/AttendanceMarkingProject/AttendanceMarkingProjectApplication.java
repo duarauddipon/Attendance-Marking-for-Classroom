1206cd29728fb54394bdf9bef7cf69195e3d12e3
Uddipon Duara <duarauddipon@gmail.com>
2022-03-14 17:55:23 +0530

    Initial Commit
    Pushed the skeleton code

---
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/AttendanceMarkingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.AttendanceMarkingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceMarkingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceMarkingProjectApplication.class, args);
+		System.out.println("Server started...");
+	}
+
+}
---
+++ AttendanceMarkingProject/src/test/java/com/AttendanceMarkingProject/AttendanceMarkingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AttendanceMarkingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceMarkingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
