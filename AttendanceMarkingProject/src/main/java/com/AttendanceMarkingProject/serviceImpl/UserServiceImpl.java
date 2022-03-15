868ce67ae4ce619d6075a13bf821e983b9d73770
Uddipon Duara <duarauddipon@gmail.com>
2022-03-15 13:26:02 +0530

    Update UserServiceImpl.java
    Added registeruser method

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
@@ -15,8 +15,24 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public String registeruser(User user) {
-		String str="insert into ";
-		return null;
+		String str="insert into userreg(firstname,lastname,empid,email,password) values(?,?,?,?,?);";
+		try
+		{
+			int res=jt.update(str, new Object[] {user.getFirstName(),user.getLastName(),user.getEmpId(),user.getEmail(),user.getPassword()});
+			if(res>=1)
+			{
+				return "User added successfully";
+			}
+			else
+			{
+				return "User not added!";
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		return "User not added!";
 	}
 
 }
