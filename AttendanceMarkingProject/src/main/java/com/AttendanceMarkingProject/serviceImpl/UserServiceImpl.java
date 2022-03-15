1dbcbf0a36545bef2eb020549792972b49e45182
Uddipon Duara <duarauddipon@gmail.com>
2022-03-15 12:57:58 +0530

    Update UserServiceImpl.java

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
@@ -1,13 +1,21 @@
 package com.AttendanceMarkingProject.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
 import com.AttendanceMarkingProject.model.User;
 import com.AttendanceMarkingProject.service.UserService;
 
+@Component
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	JdbcTemplate jt;
 
 	@Override
 	public String registeruser(User user) {
-		// TODO Auto-generated method stub
+		String str="insert into ";
 		return null;
 	}
 
