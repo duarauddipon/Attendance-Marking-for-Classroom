bc0c913af1ff2d8342150c156e59c37f7197fce2
Uddipon Duara <duarauddipon@gmail.com>
2022-03-15 12:46:39 +0530

    Create UserServiceImpl.java

---
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.AttendanceMarkingProject.serviceImpl;
+
+import com.AttendanceMarkingProject.model.User;
+import com.AttendanceMarkingProject.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public String registeruser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
