f69c31f7c38aa5347645a9d78378f7f5ed3ba516
Nithish Kumar <phoenixnithish2112@gmail.com>
2022-03-15 11:32:11 +0530

    Admin

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/model/Admin.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/model/Admin.java
@@ -1,6 +1,76 @@
 package com.AttendanceMarkingProject.model;
 
 public class Admin {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	private String number;
+	private int adminId;
+	private String password;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Admin(String firstName, String lastName, int age, String gender, String number, int adminId,
+			String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender = gender;
+		this.number = number;
+		this.adminId = adminId;
+		this.password = password;
+	}
+	public Admin() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Admin [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", gender=" + gender
+				+ ", number=" + number + ", adminId=" + adminId + ", password=" + password + "]";
+	}
+	
+	
 	
 
 }
