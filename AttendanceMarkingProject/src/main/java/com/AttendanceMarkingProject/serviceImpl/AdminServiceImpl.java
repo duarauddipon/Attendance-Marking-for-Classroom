a0169e43bda120a6e05d2e0ba20d9a6bee348c97
Nithish Kumar <phoenixnithish2112@gmail.com>
2022-03-15 12:49:31 +0530

    Update.adminservice&adminserviceImpl

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/AdminServiceImpl.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/AdminServiceImpl.java
@@ -1,14 +1,35 @@
 package com.AttendanceMarkingProject.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.AttendanceMarkingProject.model.Admin;
 import com.AttendanceMarkingProject.service.AdminService;
 
 public class AdminServiceImpl implements AdminService {
+	
+	@Autowired
+	JdbcTemplate jt;
 
 	@Override
-	public String registerAdmin(Admin admin) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean registerAdmin(Admin admin) {
+		String sql = "Insert into adminreg(firstname,lastname,age,gender,number,adminid,password)values(?,?,?,?,?,?,?)";
+		try
+		{
+			int a = jt.update(sql,new Object[] {admin.getFirstName(),admin.getLastName(),admin.getAge(),admin.getGender(),
+					admin.getNumber(),admin.getAdminId(),admin.getPassword()});
+			if(a>=1)
+			{
+				return true;
+			}
+			else
+				return false;
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		return false;
 	}
 
 }
