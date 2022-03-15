171881fde3303ec571b2daf881ff0a63e16987cf
Vignesh <95069070+VIGNESHMANII@users.noreply.github.com>
2022-03-15 13:17:54 +0530

    trainerServiceImpl

---
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/service/TrainerService.java
@@ -0,0 +1,9 @@
+package com.AttendanceMarkingProject.service;
+
+import com.AttendanceMarkingProject.model.Trainer;
+
+public interface TrainerService {
+	
+	public String addTrainer(Trainer trainer);
+
+}
