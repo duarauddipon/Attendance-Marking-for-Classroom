421574582169e5199223d425f76d990e123f9574
Nithish Kumar <phoenixnithish2112@gmail.com>
2022-03-15 12:25:04 +0530

    Create AdminServiceImpl.java

---
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/AdminServiceImpl.java
@@ -0,0 +1,14 @@
+package com.AttendanceMarkingProject.serviceImpl;
+
+import com.AttendanceMarkingProject.model.Admin;
+import com.AttendanceMarkingProject.service.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+
+	@Override
+	public String registerAdmin(Admin admin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
