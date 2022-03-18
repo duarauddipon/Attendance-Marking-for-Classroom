9b4efb5e3e1435748bc08f3c4e00c344644075fb
Nithish Kumar <phoenixnithish2112@gmail.com>
2022-03-18 12:49:44 +0530

    Update AdminService.java

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/service/AdminService.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/service/AdminService.java
@@ -4,5 +4,5 @@ import com.AttendanceMarkingProject.model.Admin;
 
 public interface AdminService {
 	public String registerAdmin(Admin admin);
-	public Admin validateAdmin(int adminId,String password); 
+	public Admin validateAdmin(int adminId,String password);
 }
