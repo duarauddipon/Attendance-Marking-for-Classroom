9dcce1c34bfd59a9882b9e80652151f928ee77f5
Uddipon Duara <duarauddipon@gmail.com>
2022-03-22 17:49:28 +0530

    Cleaning up

--- AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
+++ AttendanceMarkingProject/src/main/java/com/AttendanceMarkingProject/serviceImpl/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.AttendanceMarkingProject.serviceImpl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -54,32 +52,4 @@ public class UserServiceImpl implements UserService {
 		}
 		return null;
 	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public List<Session> showAllSession() {
-		String str ="select * from sessiondet;";
-		try {
-			List<Session> slist = jt.query(str, new BeanPropertyRowMapper(Session.class)) ;
-			return slist;
-		}catch(Exception ex) {
-			ex.getMessage();
-		}
-		return null;
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public Session searchSession(int sessionId) {
-		String sql="select * from sessiondet where sessionid=?";
-		try {
-			@SuppressWarnings("deprecation")
-			Session dlist = (Session) jt.queryForObject(sql, new Object[] {sessionId}, new BeanPropertyRowMapper(Session.class));
-			return dlist;
-		}catch(Exception ex) {
-			ex.getMessage();
-		}
-		return null;	}
-
-	
 }
